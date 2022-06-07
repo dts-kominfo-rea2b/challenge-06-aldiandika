0d0aebb1874ac10b0ab998ea5b3ab48f19171d11
Aldi Andika <aldi.andika@gmail.com>
2022-06-07 21:46:39 +0700

    Turn in challenge 6

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,34 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+
+const bacaData =  (fnCallback) => {
+
+  // Baca file1
+  fs.readFile(file1, 'utf-8', (err, data1) => {
+    if (err){
+      return err;
+    }
+    let hasil1 = JSON.parse(data1);
+    let res1 = hasil1.message.split(" ")[1];
+
+    fs.readFile(file2,'utf-8',(err, data2)=>{
+      if (err){
+        return err;
+      }
+      let hasil2 = JSON.parse(data2);
+      let res2 = hasil2[0].message.split(" ")[1];
+
+      fs.readFile(file3,'utf-8',(err, data3)=>{
+        let hasil3 = JSON.parse(data3);
+        let res3 = hasil3[0].data.message.split(" ")[1];
+
+        let res = [ res1, res2, res3];
+        fnCallback(err, res);
+      })
+    })
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
